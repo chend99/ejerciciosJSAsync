1f5c061289cd59c397c4c8f47d7a2272564654cb
chend99 <chendavid99@hotmail.com.ar>
2020-09-11 00:06:41 -0300

    Ejercicios 5 y 6 Hechos

--- ejercicio_6/app.js
+++ ejercicio_6/app.js
@@ -2,4 +2,18 @@
 const obtenerPersonaFake = require("./library");
 
 // Usar la funcion obtenerPersonaFake() la cual devuelve la promesa de traer el objeto persona extraido
-obtenerPersonaFake().then(x => console.log(x));
+obtenerPersonaFake().then((p) => {
+    let persona = p.results[0];
+    let nombre = persona.name;
+    let localidad = persona.location;
+    let user = persona.login;
+    console.log(
+    `
+    Nombre: ${nombre.first} ${nombre.last}
+    Género: ${persona.gender==="male"?"Masculino":"Femenino"}
+    Pais: ${localidad.country}, Estado: ${localidad.state}, Ciudad: ${localidad.city}, Calle: ${localidad.street.name}, Nro: ${localidad.street.number}, CP: ${localidad.postcode}                
+    Email: ${persona.email}
+    Nombre de usuario: ${user.username}
+    Password: ${user.password}
+    `);
+});
