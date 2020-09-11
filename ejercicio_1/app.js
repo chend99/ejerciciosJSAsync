606ef86e8ae91b21fbc8d1657f7f77503a6dbe49
chend99 <chendavid99@hotmail.com.ar>
2020-09-10 22:41:06 -0300

    Ejercicios 1 y 2 Hechos

--- ejercicio_1/app.js
+++ ejercicio_1/app.js
@@ -2,10 +2,10 @@
 const obtenerChiste = require("./library");
 
 // Codigo funcion callback
-
-
-
+function chisteExtraido(chiste){
+    console.log(chiste[0].setup + "\n" + chiste[0].punchline);
+}
 // Fin codigo
 
 // Usar la funcion obtenerChiste(funcionCallback) en donde funcionCallback es una función que recibe el objeto chiste extraido
-obtenerChiste();
+obtenerChiste(chisteExtraido);
--- ejercicio_2/app.js
+++ ejercicio_2/app.js
@@ -2,4 +2,7 @@
 const obtenerChiste = require("./library");
 
 // Usar la funcion obtenerChiste() la cual devuelve la promesa de traer el objeto chiste extraido
-obtenerChiste();
+obtenerChiste().then((chiste)=>{
+    console.log(chiste[0].setup + "\n" + chiste[0].punchline);
+});
+
