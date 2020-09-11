1e0272602b11cac53e736c53a9f3d512b535a873
chend99 <chendavid99@hotmail.com.ar>
2020-09-10 22:53:49 -0300

    Ejercicios 3 y 4 Hechos

--- ejercicio_3/app.js
+++ ejercicio_3/app.js
@@ -2,13 +2,13 @@
 const obtenerPokemon = require("./library");
 
 // Codigo funcion callback
-
-
-
+function traerNombre(pokemon){
+    console.log(pokemon.name);
+}
 // Fin codigo
 
 // Usar la funcion obtenerPokemon(pokemon,funcionCallback) en donde:
 // - nombrePokemon es un string con el nombre del pokemon
 // - funcionCallback es una función que recibe el objeto con los datos del pokemon extraido
 
-obtenerPokemon();
+obtenerPokemon('pikachu',traerNombre);
--- ejercicio_4/app.js
+++ ejercicio_4/app.js
@@ -2,4 +2,6 @@
 const obtenerPokemon = require("./library");
 
 // Usar la funcion obtenerChiste(nombrePokemon) la cual recibe como string un nombre de pokemon y devuelve la promesa de traer el objeto pokemon extraido
-obtenerPokemon();
+obtenerPokemon('bulbasaur').then((pokemon)=>{
+    console.log(pokemon.name);
+});
